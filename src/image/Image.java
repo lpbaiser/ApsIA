f0404b2d50669ac831e995bba92f09c5d0122a57
Rômulo Manciola Meloca <rmeloca@gmail.com>
2016-06-26 15:52:17 -0300

    aaa

--- src/image/Image.java
+++ src/image/Image.java
@@ -11,8 +11,6 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 
 /**
