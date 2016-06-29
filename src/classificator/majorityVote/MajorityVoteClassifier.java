f64bb06db3e92b390ece001e935f054cd88684f7
Rômulo Manciola Meloca <rmeloca@gmail.com>
2016-06-29 02:52:54 -0300

    zuei

--- src/classificator/majorityVote/MajorityVoteClassifier.java
+++ src/classificator/majorityVote/MajorityVoteClassifier.java
@@ -26,7 +26,6 @@ public class MajorityVoteClassifier implements Classifier {
     private Conjunto teste;
     private final Classifier knn;
     private Confusao confusao;
-    private svm svm;
     private DecisionTree decisionTree;
 
     public MajorityVoteClassifier(Conjunto treino) {
@@ -36,9 +35,7 @@ public class MajorityVoteClassifier implements Classifier {
         } catch (Exception ex) {
             throw new RuntimeException("Impossível inicializar jogador");
         }
-        svm = new svm();
         decisionTree = new DecisionTree();
-        decisionTree.
     }
 
     @Override
